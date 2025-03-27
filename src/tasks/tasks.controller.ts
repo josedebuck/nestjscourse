00747e173f00be17e95bb4584ae237529813a72a
josedebuck <debuckjose@gmail.com>
2025-03-27 17:36:48 -0300

    agregando funcionalidades en Controllers y Service

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,13 +1,43 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Delete, Get, Patch, Post, Put, Body, Query, Param } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 
-@Controller({})
+@Controller('/tasks')
 export class TaskController {
     constructor(private tasksService:TasksService){
     }
 
-    @Get('/tasks')
-    getAllTasks(){
+    // Get de todas las tareas
+    @Get()
+    getAllTasks(@Query() query: any){
+        console.log(query)
        return this.tasksService.getTasks();
     }
+
+    // Get de una tarea especifica
+    @Get('/:id')
+    getTask(@Param('id') id: string){
+        console.log(id)
+       return this.tasksService.getTask(parseInt(id));
+    }
+
+    @Post()
+    createTask(@Body() task: any){
+        return this.tasksService.createTask(task);
+    }
+
+    @Put() // {title: 'primera tarea', status: false} -> {title: "tarea actualizando", status: true}
+    updateTask(){
+        return this.tasksService.updateTask;
+    }
+
+    @Delete()
+    deleteTask(){
+        return this.tasksService.deleteTask;
+    }
+
+    @Patch() // {title: 'primera tarea', status: false} -> {status: true}
+    patchTask(){
+        return this.tasksService.updateTaskStatus;
+    }
+
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,9 +1,48 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+
+export interface User {
+  name: string;
+  age: number;
+}
 
 @Injectable()
 export class TasksService {
-    getTasks() {
-        return [ 'Task1','Task2','Task3']
-    }
+    private tasks: any[] = []; // aqui se guardan las tareas que se reciben
 
+  //Función para obtener todas las tareas
+  getTasks() {
+    return this.tasks;
+  }
+
+  //Función para obtener una tarea específica
+  getTask(id: number) {
+    const taskFound = this.tasks.find(task => task.id === id);
+
+    if(!taskFound){
+        throw new NotFoundException(`Tarea con el id ${id} no encontrada`);
+    }
+    return taskFound
+  }
+
+
+  createTask(task: any){
+    console.log(task)
+    this.tasks.push({
+    ...task, // copia todos los valores de tareas
+        id: this.tasks.length + 1 // y le coloco un ID en + 1
+    });
+    return task
+  }
+
+  updateTask() {
+    return 'actualizando tareas';
+  }
+
+  deleteTask() {
+    return 'eliminando tareas';
+  }
+
+  updateTaskStatus() {
+    return 'actualizando parcialmente tareas';
+  }
 }
